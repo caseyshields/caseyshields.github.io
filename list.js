50326c0e34c615d854402aed6ac2307547d288f9
caseyshields <caseyorigami@gmail.com>
2018-01-03 18:25:33 -0800

    working on diagram template

--- list.js
+++ list.js
@@ -19,6 +19,7 @@ Vue.component('entry', {
   <h2>{{name}}</h2>
   <ul><li v-for='tag in tags'>{{tag}}</li></ul>
   <div v-text="content"></div>
+  <ul><li v-for='count in 10'>{{count}}</li></ul>
   </article>`
   // todo create content based on
 });
@@ -28,10 +29,14 @@ Vue.component('diagrams', {
     name: {type: String, required: true},
     stages: {type: Array, required: true}
   },
-  template: `<section>
-  <figure v-for="count in stages">
-  <img v-bind:src="'name_'+count"/>
-  </figure>
+  data: {
+    // todo extrapolate the comments into an object then use it with teh template.
+  },
+  template:
+  `<section v-for="stage in stages">
+    <figure v-for="step in stage.steps">
+      <img v-bind:src="name+'/'+name+'_'stage+'_'+step"/>
+    </figure>
   </section>`
 });
 
@@ -43,6 +48,18 @@ let instance = new Vue({
     articles: []
   },
   created: function() {
+    //stages = {1:5, 2:5, 3:10, 4:11, 5:8, 6:8, 7:10, 8:7, 9:3, 10:6, 11:4};
+    stages = [ {
+        stage: 1, 
+        steps: 5,
+        captions:{1:"first caption"}
+      },{
+        stage: 2, 
+        steps: 5,
+        captions:{1:"first caption"}
+      }
+    ];
+    stages = [5, 5, 10, 11, 8, 8, 10, 7, 3, 6, 4];
     let test = {
       name: 'Western Dragon',
       type: 'origami',
