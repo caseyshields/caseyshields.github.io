7a96d24d02d9c5b24f9031c32e3a739aa25d2cd0
caseyshields <caseyorigami@gmail.com>
2018-01-04 14:10:00 -0800

    Got diagrams working in vue.

--- list.js
+++ list.js
@@ -19,25 +19,28 @@ Vue.component('entry', {
   <h2>{{name}}</h2>
   <ul><li v-for='tag in tags'>{{tag}}</li></ul>
   <div v-text="content"></div>
-  <ul><li v-for='count in 10'>{{count}}</li></ul>
   </article>`
   // todo create content based on
 });
 
-Vue.component('diagrams', {
+Vue.component('diagram', {
   props: {
     name: {type: String, required: true},
     stages: {type: Array, required: true}
   },
-  data: {
-    // todo extrapolate the comments into an object then use it with teh template.
-  },
+  // data: function() {
+  //   // todo extrapolate the comments into an object then use it with teh template.
+  // },
   template:
-  `<section v-for="stage in stages">
-    <figure v-for="step in stage.steps">
-      <img v-bind:src="name+'/'+name+'_'stage+'_'+step"/>
-    </figure>
-  </section>`
+  `<article>
+    <section v-for="stage in stages">
+      <div>{{stage.summary}}</div>
+      <figure v-for="step in stage.steps">
+        <img :src="name + '/' + name + '_' + stage.stage + '_' + step + '.svg'">
+        <figcaption>TODO insert the captions here</figcaption>
+      </figure>
+    </section>
+  </article>`
 });
 
 let instance = new Vue({
@@ -45,27 +48,30 @@ let instance = new Vue({
   data: {
     title: "Vue Component Examples",
     query: '',
-    articles: []
+    articles: [],
+    stages: []
   },
   created: function() {
     //stages = {1:5, 2:5, 3:10, 4:11, 5:8, 6:8, 7:10, 8:7, 9:3, 10:6, 11:4};
-    stages = [ {
-        stage: 1, 
-        steps: 5,
-        captions:{1:"first caption"}
-      },{
-        stage: 2, 
-        steps: 5,
-        captions:{1:"first caption"}
-      }
-    ];
-    stages = [5, 5, 10, 11, 8, 8, 10, 7, 3, 6, 4];
-    let test = {
+    // stages = [5, 5, 10, 11, 8, 8, 10, 7, 3, 6, 4];
+    this.stages.push( {
+      stage: 1, 
+      steps: 5,
+      summary: "Construct the landmark sequences",
+      captions: {1:"first caption"}
+    });
+    this.stages.push( {
+      stage: 2, 
+      steps: 5,
+      summary: "fold a birdbase with a grafted diagonal strip",
+      captions: {1:"first caption"}
+    });
+
+    this.articles.push( {
       name: 'Western Dragon',
       type: 'origami',
       tags: ['intermediate', 'dragon', 'half-angle'],
       content: 'do as they do, not as they say'
-    }
-    this.articles.push( test );
+    });
   }
 });
